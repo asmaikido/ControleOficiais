a48379e784b36b76daccc240be6d02e6b104c5d6
Alexandre <asmac72@gmail.com>
2015-07-15 09:39:10 -0300

    Meu quarto commit
    Criação de Statement

--- src/main/java/jar/persistencia/jdbc/ConexaoFactory.java
+++ src/main/java/jar/persistencia/jdbc/ConexaoFactory.java
@@ -1,12 +1,18 @@
 package jar.persistencia.jdbc;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConexaoFactory {
 
 	public static Connection getConnection() {
 		
-		return DriverManager.getConnection();
+		try {
+			return DriverManager.getConnection("jdbc.postgresql://localhost:5432/Teste","postgres","postgres");
+		} catch (SQLException e) {
+			// relançando a exception
+			throw new RuntimeException(e);
+		}
 	}
-
 }
--- src/main/java/jar/persistencia/jdbc/OficialDAO.java
+++ src/main/java/jar/persistencia/jdbc/OficialDAO.java
@@ -1,15 +1,30 @@
 package jar.persistencia.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import jar.persistencia.entidade.Oficial;
 
 public class OficialDAO {
-
+	private Connection conexao = ConexaoFactory.getConnection();
+	
 	public void cadastrar(Oficial oficial) {
-		
-		private Connection conexao = ConexaoFactory.getConnection();
-		
 		String sql = "insert into oficial (of_matricula, of_nome) values (?,?)";
 		
+		try {
+			//Criando um Statement
+			PreparedStatement preparador = conexao.prepareStatement(sql);
+			preparador.setString(1, oficial.getOf_nome());
+			preparador.setString(2, oficial.getOf_matricula());
+			//Executando o comando SQL no banco
+			preparador.execute();
+			//Fechando o objeto preparador
+			preparador.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
